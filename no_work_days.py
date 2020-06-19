3475cd5731ab174829a2ad84167dee00724b64b4
Melnikov71 <67158330+Melnikov71@users.noreply.github.com>
2020-06-19 17:56:36 +0300

    Add files via upload

---
+++ no_work_days.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import date,datetime
+import re
+
+
+def consultantNoWorkDays():
+    no_work_days=[]
+    url=f'http://www.consultant.ru/law/ref/calendar/proizvodstvennye/{date.today().year}/'
+    consultant_html=requests.get(url).text
+
+    soup = BeautifulSoup(consultant_html, 'html.parser')
+    searchResult_block = soup.findAll('table', {'class':'cal'})
+    
+    if searchResult_block!=None:
+        for index, block in enumerate(searchResult_block):
+            for weekend in block.find_all('td', class_='weekend'):
+                day=re.findall(r'\d+', weekend.get_text())[0]
+                no_work_days.append(datetime.strptime(f'{day}.{index+1}.{date.today().year}', '%d.%m.%Y').date())
+
+    return no_work_days
+
+print(consultantNoWorkDays())
