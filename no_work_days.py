eb313e7305b880b831aa0daba00c0b370aa2a666
Melnikov71 <67158330+Melnikov71@users.noreply.github.com>
2020-06-21 18:10:18 +0300

    Update no_work_days.py

--- no_work_days.py
+++ no_work_days.py
@@ -7,17 +7,17 @@ import re
 def consultantNoWorkDays():
     no_work_days=[]
     url=f'http://www.consultant.ru/law/ref/calendar/proizvodstvennye/{date.today().year}/'
-    consultant_html=requests.get(url).text
-
-    soup = BeautifulSoup(consultant_html, 'html.parser')
-    searchResult_block = soup.findAll('table', {'class':'cal'})
+    response=requests.get(url)
+    if response.status_code == 200:
+        consultant_html=response.text
+        soup = BeautifulSoup(consultant_html, 'html.parser')
+        searchResult_block = soup.findAll('table', {'class':'cal'})
     
-    if searchResult_block!=None:
-        for index, block in enumerate(searchResult_block):
-            for weekend in block.find_all('td', class_='weekend'):
-                day=re.findall(r'\d+', weekend.get_text())[0]
-                no_work_days.append(datetime.strptime(f'{day}.{index+1}.{date.today().year}', '%d.%m.%Y').date())
+        if searchResult_block!=None:
+            for index, block in enumerate(searchResult_block):
+                for weekend in block.find_all('td', class_='weekend'):
+                    day=re.findall(r'\d+', weekend.get_text())[0]
+                    no_work_days.append(datetime.strptime(f'{day}.{index+1}.{date.today().year}', '%d.%m.%Y').date())
+    return {'days_count': len(no_work_days), 'days': no_work_days}
 
-    return no_work_days
-
-print(consultantNoWorkDays())
+print(date.today() in consultantNoWorkDays()['days'])
